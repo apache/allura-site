86ab4a2486a5e861da57c4d63716838a894f4844
Dave Brondsema <dave@brondsema.net>
2018-02-06 12:52:19 -0500

    Allura 1.8.0 release

--- _src/pelicanconf.py
+++ _src/pelicanconf.py
@@ -44,9 +44,9 @@ TAG_FEED_ATOM = 'feeds/tag.%s.atom.xml'
 
 CURRENT_YEAR = dt.date.today().year
 
-RELEASE_VERSION = '1.7.0'
-RELEASE_DATE = 'Jun 2017'
-RELEASE_NEWS = 'posts/2017-allura-1.7.0.html'
+RELEASE_VERSION = '1.8.0'
+RELEASE_DATE = 'Feb 2018'
+RELEASE_NEWS = 'posts/2018-allura-1.8.0.html'
 DIST_URL = 'http://apache.org/dist/allura/'
 
 FORGE_ALLURA_URL = 'https://forge-allura.apache.org/'
